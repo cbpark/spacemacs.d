04499c3bc2cbc468599292b9ce02620a1a7ccb34
cbpark <cbpark@gmail.com>
2021-12-02 14:09:03 +0900

    Append auto-mode-alist

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -17,6 +17,23 @@
 (with-eval-after-load 'markdown-mode
   (add-hook 'markdown-mode-hook 'turn-off-auto-fill))
 
+;; syntax highlighting for sysmtemd files
+(add-to-list 'auto-mode-alist '("\\.service\\'"   . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.timer\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.target\\'"    . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.mount\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.slice\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.socket\\'"    . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.path\\'"      . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.netdev\\'"    . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.network\\'"   . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.link\\'"      . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
+
+;; PKGBUILD files
+(add-to-list 'auto-mode-alist '("/PKGBUILD" . shell-script-mode))
+
 (global-hl-line-mode -1)
 
 (provide 'init-misc)
