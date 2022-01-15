36d1933579d89dae836cb45cac711835fb4010f1
cbpark <cbpark@gmail.com>
2022-01-15 19:05:07 +0900

    Do not trigger org when launching

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,16 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(dolist (hook '(visual-line-mode
-                turn-off-auto-fill))
-  (add-hook 'org-mode-hook hook))
-
-;; show inline images
-(add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
-
-;; markdown
 (with-eval-after-load 'org
-  (require 'ox-md nil t))
+  ;; show inline images
+  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
+
+  (dolist (hook '(visual-line-mode
+                  turn-off-auto-fill))
+    (add-hook 'org-mode-hook hook)))
 
 (provide 'init-org)
 ;;; init-org.el ends here
