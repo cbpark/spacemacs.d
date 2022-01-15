72111b83d4d51b32112b0f6e42440273380841bb
cbpark <cbpark@gmail.com>
2022-01-15 18:47:28 +0900

    Add hooks to TeX-mode

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -5,9 +5,12 @@
 ;; PDF viewer
 (when (executable-find "okular")
   (with-eval-after-load "tex"
-    (setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) "Okular")))
+    (setcar (cdr (assoc 'output-pdf TeX-view-program-selection))
+            "Okular")))
 
-(add-hook 'TeX-mode-hook #'display-line-numbers-mode)
+(dolist (hook '(display-line-numbers-mode
+                visual-line-mode))
+  (add-hook 'TeX-mode-hook hook))
 
 (provide 'init-auctex)
 ;;; init-auctex.el ends here
