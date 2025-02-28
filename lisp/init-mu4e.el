3add7950a378993eaa9385207a40b7a716464fb8
cbpark <cbpark@gmail.com>
2025-02-28 16:20:24 +0900

    Update init-mu4e (remove jnu)

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -21,22 +21,14 @@
       '(("/gmail/INBOX"                   . ?g)
         ("/gmail/[Google Mail]/Sent Mail" . ?s)
         ("/gmail/[Google Mail]/Trash"     . ?t)
-        ("/gmail/arXiv-hep"               . ?a)
-        ("/jnu/Inbox"                     . ?j)
-        ("/jnu/Sent"                      . ?e)
-        ("/jnu/Trash"                     . ?r)))
+        ("/gmail/arXiv-hep"               . ?a)))
 
 (defvar my-mu4e-account-alist
   '(("gmail"
      (mu4e-sent-folder "/gmail/[Google Mail]/Sent Mail")
      (mu4e-drafts-folder "/gmail/[Google Mail]/Drafts")
      (mu4e-trash-folder "/gmail/[Google Mail]/Trash")
-     (user-mail-address "cbpark@gmail.com"))
-    ("jnu"
-     (mu4e-sent-folder "/jnu/Sent")
-     (mu4e-drafts-folder "/jnu/Drafts")
-     (mu4e-trash-folder "/jnu/Trash")
-     (user-mail-address "cbpark@jnu.ac.kr"))))
+     (user-mail-address "cbpark@gmail.com"))))
 
 (defun my-mu4e-set-account ()
   "Set the account for composing a message."
@@ -55,7 +47,7 @@
          (account-vars (cdr (assoc account my-mu4e-account-alist))))
     (if account-vars
         (mapc (lambda (var)
-                  (set (car var) (cadr var)))
+                (set (car var) (cadr var)))
               account-vars)
       (error "No email account found"))))
 
@@ -95,34 +87,34 @@
       user-mail-address "cbpark@gmail.com")
 
 ;; use the address in the message when replying.
-(defun my-set-from-address ()
-  "Set the From address based on the To address of the original."
-  (let ((msg mu4e-compose-parent-message))
-    (if msg
-        (setq user-mail-address
-              (cond ((mu4e-message-contact-field-matches
-                      msg :to "cbpark@jnu.ac.kr") "cbpark@jnu.ac.kr")
-                    (t "cbpark@gmail.com"))))))
-(add-hook 'mu4e-compose-pre-hook 'my-set-from-address)
+;; (defun my-set-from-address ()
+;;   "Set the From address based on the To address of the original."
+;;   (let ((msg mu4e-compose-parent-message))
+;;     (if msg
+;;         (setq user-mail-address
+;;               (cond ((mu4e-message-contact-field-matches
+;;                       msg :to "cbpark@jnu.ac.kr") "cbpark@jnu.ac.kr")
+;;                     (t "cbpark@gmail.com"))))))
+;; (add-hook 'mu4e-compose-pre-hook 'my-set-from-address)
 
 ;; MSMTP setting for multi-smtp sending
 (setq message-send-mail-function 'message-send-mail-with-sendmail)
 (setq sendmail-program "msmtp")
 
-(defun choose-msmtp-account ()
-  "Choose account label to feed msmtp -a option based on From header."
-  (if (message-mail-p)
-      (save-excursion
-        (let* ((from (save-restriction
-                       (message-narrow-to-headers)
-                       (message-fetch-field "from")))
-               (account (cond ((string-match "cbpark@gmail.com" from)
-                               "cbpark@gmail.com")
-                              ((string-match "cbpark@jnu.ac.kr" from)
-                               "cbpark@jnu.ac.kr"))))
-          (setq message-sendmail-extra-arguments (list '"-a" account))))))
-(setq message-sendmail-envelope-from 'header)
-(add-hook 'message-send-mail-hook 'choose-msmtp-account)
+;; (defun choose-msmtp-account ()
+;;   "Choose account label to feed msmtp -a option based on From header."
+;;   (if (message-mail-p)
+;;       (save-excursion
+;;         (let* ((from (save-restriction
+;;                        (message-narrow-to-headers)
+;;                        (message-fetch-field "from")))
+;;                (account (cond ((string-match "cbpark@gmail.com" from)
+;;                                "cbpark@gmail.com")
+;;                               ((string-match "cbpark@jnu.ac.kr" from)
+;;                                "cbpark@jnu.ac.kr"))))
+;;           (setq message-sendmail-extra-arguments (list '"-a" account))))))
+;; (setq message-sendmail-envelope-from 'header)
+;; (add-hook 'message-send-mail-hook 'choose-msmtp-account)
 
 ;; don't keep message buffers around.
 (setq message-kill-buffer-on-exit t)
