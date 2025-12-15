2659f58ef4044a445e7077ec348bbc8779ddc5f5
cbpark <cbpark@gmail.com>
2025-12-15 22:13:57 +0900

    Fix for gnuplot

--- init.el
+++ init.el
@@ -90,7 +90,7 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '((gnuplot :location "/usr/share/emacs/site-lisp/"))
+   dotspacemacs-additional-packages '(gnuplot)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
